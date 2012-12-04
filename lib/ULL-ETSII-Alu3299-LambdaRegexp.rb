15d9323f4bd3c57dfdfe1f64a2ed99f176b595a8
mdbgalvan <magomenlopark@gmail.com>
2012-12-04 05:44:57 +0000

    modulo para las ER

--- lib/ULL-ETSII-Alu3299-LambdaRegexp.rb
+++ lib/ULL-ETSII-Alu3299-LambdaRegexp.rb
@@ -1,10 +1,13 @@
+# File : ULL-ETSII-Alu3299-LambdaRegexp.rb
+
 require "ULL-ETSII-Alu3299-LambdaRegexp/version"
+require "ULL-ETSII-Alu3299-LambdaRegexp/lambdaRegexp.rb"
 
 module ULL
   module ETSII
     module Alu3299
       module LambdaRegexp
-        # Your code goes here...
+
       end
     end
   end
